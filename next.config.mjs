c897848712366f64d1dd95df46d12ec03625956a
李爽 <14976992+li-1993-shuang@user.noreply.gitee.com>
2024-10-13 14:03:16 +0800

    fix:修改重定向

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
 	async redirects() {
 		return [
 			{
-				source: '/home',
-				destination: '/',
+				source: '/',
+				destination: '/home',
 				permanent: true,
 			},
 		]
